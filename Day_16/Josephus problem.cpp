4273e8aa36db3486089337c9bf71f1f9ae9faf3b
Chinmay Kulkarni <79635843+chinmaykulkarni19@users.noreply.github.com>
2021-12-17 10:30:22 +0530

    Add files via upload

---
+++ Day_16/Josephus problem.cpp
@@ -0,0 +1,44 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+ // } Driver Code Ends
+/*You are required to complete this method */
+
+class Solution
+{
+    public:
+    int josephus(int n, int k)
+    {
+       return myjos(n,k)+1;
+    }
+    int myjos(int n,int k)
+    {
+        if(n==1)
+        return 0;
+        
+        return (myjos(n-1,k)+k)%n;
+    }
+};
+
+
+
+// { Driver Code Starts.
+
+int main() {
+	
+	int t;
+	cin>>t;//testcases
+	while(t--)
+	{
+		int n,k;
+		cin>>n>>k;//taking input n and k
+		
+		//calling josephus() function
+		Solution ob;
+		cout<<ob.josephus(n,k)<<endl;
+	}
+	return 0;
+}  // } Driver Code Ends
