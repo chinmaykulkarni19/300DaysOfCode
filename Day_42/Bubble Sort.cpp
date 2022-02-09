42f016868da628abf21e22243f871d4caeb4ac2f
Chinmay Kulkarni <79635843+chinmaykulkarni19@users.noreply.github.com>
2022-02-09 20:55:29 +0530

    Add files via upload

---
+++ Day_42/Bubble Sort.cpp
@@ -0,0 +1,24 @@
+class Solution
+{
+    public:
+    //Function to sort the array using bubble sort algorithm.
+    void bubbleSort(int arr[], int n)
+    {
+        bool swapped;
+        for(int i=0;i<n;i++)
+        {
+            bool swapped=false;
+            for(int j=0;j<n-i-1;j++)
+            {
+                if(arr[j]>arr[j+1]){
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                    swapped=true;
+                }
+            }
+            if(swapped==false)
+            break;
+        }
+    }
+};
