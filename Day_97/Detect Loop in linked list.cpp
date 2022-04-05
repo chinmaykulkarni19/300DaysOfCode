02af6b990b009ad1f9301b5420f30494ab1b4d35
Chinmay Kulkarni <79635843+chinmaykulkarni19@users.noreply.github.com>
2022-04-05 17:50:45 +0530

    Add files via upload

---
+++ Day_97/Detect Loop in linked list.cpp
@@ -0,0 +1,19 @@
+class Solution
+{
+    public:
+    //Function to check if the linked list has a loop.
+    bool detectLoop(Node* head)
+    {
+        Node *slow=head;
+        Node *fast=head;
+        while(fast!=NULL && fast->next!=NULL)
+        {
+            slow=slow->next;
+            fast=fast->next->next;
+            if(slow==fast)
+            return true;
+        }
+        return false;
+        
+    }
+};
