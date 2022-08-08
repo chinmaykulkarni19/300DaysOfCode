4d879ab02bb1c635a364f81de9c8cbdcfdefb4af
Chinmay Kulkarni <chinmay.22010250@viit.ac.in>
2022-08-08 18:46:56 +0530

    Subset Sum Problem

---
+++ Day__178/Subset Sum Problem.cpp
@@ -0,0 +1,36 @@
+class Solution{   
+public:
+    bool isSubsetSum(vector<int>arr, int sum){
+        int n=arr.size();
+        bool dp[n+1][sum+1];
+        for(int i=0;i<n+1;i++)
+        {
+            for(int j=0;j<sum+1;j++)
+            {
+                if(i==0)
+                {
+                    dp[i][j]=false;
+                }
+                if(j==0)
+                {
+                    dp[i][j]=true;
+                }
+            }
+        }
+        for(int i=1;i<n+1;i++)
+        {
+            for(int j=1;j<sum+1;j++)
+            {
+                if(arr[i-1]<=j)
+                {
+                    dp[i][j]=dp[i-1][j] || dp[i-1][j-arr[i-1]];
+                }
+                else
+                {
+                    dp[i][j]=dp[i-1][j];
+                }
+            }
+        }
+        return dp[n][sum];
+    }
+};
