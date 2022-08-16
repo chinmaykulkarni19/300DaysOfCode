bc77d2dceb687a5f4f5e44a4f7f0427b44de1855
Chinmay Kulkarni <chinmay.22010250@viit.ac.in>
2022-08-16 17:42:07 +0530

    Shortest Common Supersequence

---
+++ Day__185/Shortest Common Supersequence.cpp
@@ -0,0 +1,28 @@
+class Solution
+{
+    public:
+    //Function to find length of shortest common supersequence of two strings.
+    int shortestCommonSupersequence(string X, string Y, int m, int n)
+    {
+        int dp[m+1][n+1];
+        for(int i=0;i<m+1;i++)
+        {
+            for(int j=0;j<n+1;j++)
+            {
+                if(i==0||j==0)
+                {
+                    dp[i][j]=0;
+                }
+                else if(X[i-1]==Y[j-1])
+                {
+                    dp[i][j]=1+dp[i-1][j-1];
+                }
+                else
+                {
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        return m+n-dp[m][n];
+    }
+};
