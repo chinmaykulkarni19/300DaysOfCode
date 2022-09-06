a405b2e75f145afd12bc66aab0035f7da0a0fc11
Chinmay Kulkarni <chinmay.22010250@viit.ac.in>
2022-09-06 07:41:54 +0530

    Minimum Sum of Absolute Differences of Pairs

---
+++ Day__203/Minimum Sum of Absolute Differences of Pairs.cpp
@@ -0,0 +1,13 @@
+class Solution{
+public:
+    long long findMinSum(vector<int> &A,vector<int> &B,int N){
+        long long ans=0;
+        sort(A.begin(),A.end());
+        sort(B.begin(),B.end());
+        for(int i=0;i<N;i++)
+        {
+            ans+=abs(A[i]-B[i]);
+        }
+        return ans;
+    }
+};
